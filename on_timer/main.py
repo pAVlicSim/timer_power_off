9eb49c7b0f0690fb437a895df719075c91be4d9a
simak000 <simakoff.pasha@yandex.ru>
2022-12-26 20:29:24 +0300

    практически готово

--- on_timer/main.py
+++ on_timer/main.py
@@ -1,9 +1,16 @@
+import re
 import subprocess
 import sys
+import alsaaudio
 
 from PySide6.QtWidgets import QWidget, QApplication
 from main_form import Ui_Form
 from datetime import datetime as dt
+
+
+def get_volume_level():
+    volume_level = subprocess.run(["amixer", "-D", "pulse", "get", "Master"], stdout=subprocess.PIPE)
+    return int(re.search(r'\d{1,3}%', volume_level.stdout.decode('utf-8'))[0][:-1])
 
 
 class MainTimer(QWidget):
@@ -17,25 +24,32 @@ class MainTimer(QWidget):
         except FileNotFoundError:
             print('No timer')
 
+        self.ui.dial_sound.setValue(get_volume_level())
+        # get_volume = subprocess.run(["amixer", "-D", "pulse", "get", "Master"], stdout=subprocess.PIPE)
+        print('set_volume', subprocess.call(["amixer", "-D", "pulse", "sset", "Master", "10%"]))
+        # print('get_volume', get_volume.stdout.decode('utf-8'))
+
         self.ui.pushButton_OK.clicked.connect(self.create_command)
         self.ui.pushButton_Cancel.clicked.connect(self.stop_timer)
+        self.ui.dial_sound.valueChanged.connect(self.changed_sound_volume)
 
     def timer_number(self):
-        if self.ui.lineEdit_timer.text() == '':
+        if self.ui.dial_timer.value() == 9:
             return [self.ui.timeEdit_timer.text(), f'Компьютер выключится в {self.ui.timeEdit_timer.text()}']
         else:
-            return [self.ui.lineEdit_timer.text(), f'Компьютер выключится через {self.ui.lineEdit_timer.text()} мин']
+            return [str(self.ui.dial_timer.value()), f'Компьютер выключится через '
+                                                     f'{str(self.ui.dial_timer.value())} мин']
 
     def create_command(self):
         time_timer = self.timer_number()
         timer_start = subprocess.Popen(['shutdown', '-h', time_timer[0]], stdout=subprocess.PIPE,
                                        stderr=subprocess.STDOUT)
-        self.ui.label_3.setText(f'Компьютер выключится в {time_timer[1]}')
+        self.ui.label_status.setText(f'Компьютер выключится в {time_timer[1]}')
         print('timer_start', timer_start.communicate())
 
     def stop_timer(self):
         subprocess.run(['shutdown', '-c'])
-        self.ui.label_3.setText('Таймер выключения отключён.')
+        self.ui.label_status.setText('Таймер выключения отключён.')
 
     def stats_timer(self):
         status = subprocess.run(['busctl', 'get-property', 'org.freedesktop.login1',
@@ -44,9 +58,13 @@ class MainTimer(QWidget):
         time_list = status.stdout.decode('utf-8').split()
         print(time_list[2])
         if int(time_list[2]) == 0:
-            self.ui.label_3.setText('Таймер выключения не установлен.')
+            self.ui.label_status.setText('Таймер выключения не установлен.')
         else:
-            self.ui.label_3.setText(f"Время выключения: {dt.fromtimestamp(int(time_list[2][0:-6])): %H:%M}")
+            self.ui.label_status.setText(f"Время выключения: {dt.fromtimestamp(int(time_list[2][0:-6])): %H:%M}")
+
+    def changed_sound_volume(self):
+        subprocess.call(["amixer", "-D", "pulse", "sset", "Master", f'{self.ui.dial_sound.value()}%'],
+                        stdout=subprocess.PIPE)
 
 
 if __name__ == "__main__":
--- on_timer/main_form.py
+++ on_timer/main_form.py
@@ -15,68 +15,96 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
-    QLineEdit, QPushButton, QSizePolicy, QSlider,
+from PySide6.QtWidgets import (QApplication, QDial, QGridLayout, QHBoxLayout,
+    QLCDNumber, QLabel, QPushButton, QSizePolicy,
     QSpacerItem, QTimeEdit, QVBoxLayout, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(695, 300)
+        Form.resize(576, 503)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
         Form.setStyleSheet(u"font: 500 italic 16pt \"Yrsa Medium\";")
         self.verticalLayout = QVBoxLayout(Form)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
+        self.label_4 = QLabel(Form)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 1, 0, 1, 1)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.timeEdit_timer = QTimeEdit(Form)
+        self.timeEdit_timer.setObjectName(u"timeEdit_timer")
+        self.timeEdit_timer.setStyleSheet(u"font: italic 16pt \"Sans Serif\";")
+
+        self.horizontalLayout_3.addWidget(self.timeEdit_timer)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_3, 1, 2, 1, 1)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.dial_timer = QDial(Form)
+        self.dial_timer.setObjectName(u"dial_timer")
+        self.dial_timer.setMaximum(120)
+
+        self.horizontalLayout_2.addWidget(self.dial_timer)
+
+        self.lcdNumber_minutes = QLCDNumber(Form)
+        self.lcdNumber_minutes.setObjectName(u"lcdNumber_minutes")
+        font = QFont()
+        font.setFamilies([u"Yrsa Medium"])
+        font.setPointSize(16)
+        font.setBold(False)
+        font.setItalic(True)
+        self.lcdNumber_minutes.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.lcdNumber_minutes)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_2, 0, 2, 1, 1)
+
         self.label_2 = QLabel(Form)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
 
         self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
 
-        self.horizontalSlider = QSlider(Form)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        self.dial_sound = QDial(Form)
+        self.dial_sound.setObjectName(u"dial_sound")
+        self.dial_sound.setMaximum(100)
 
-        self.gridLayout.addWidget(self.horizontalSlider, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.dial_sound, 2, 2, 1, 1)
 
         self.label = QLabel(Form)
         self.label.setObjectName(u"label")
         self.label.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
 
-        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
-
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.lineEdit_timer = QLineEdit(Form)
-        self.lineEdit_timer.setObjectName(u"lineEdit_tiver")
-
-        self.horizontalLayout_2.addWidget(self.lineEdit_timer)
-
-        self.label_4 = QLabel(Form)
-        self.label_4.setObjectName(u"label_4")
-
-        self.horizontalLayout_2.addWidget(self.label_4)
-
-        self.timeEdit_timer = QTimeEdit(Form)
-        self.timeEdit_timer.setObjectName(u"timeEdit_timer")
-
-        self.horizontalLayout_2.addWidget(self.timeEdit_timer)
-
-
-        self.gridLayout.addLayout(self.horizontalLayout_2, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
 
 
         self.verticalLayout.addLayout(self.gridLayout)
 
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.label_3 = QLabel(Form)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setAlignment(Qt.AlignCenter)
+        self.label_status = QLabel(Form)
+        self.label_status.setObjectName(u"label_status")
+        self.label_status.setStyleSheet(u"font: italic 16pt \"Sans Serif\";")
+        self.label_status.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout_2.addWidget(self.label_3)
+        self.verticalLayout_2.addWidget(self.label_status)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
@@ -102,16 +130,17 @@ class Ui_Form(object):
 
 
         self.retranslateUi(Form)
+        self.dial_timer.valueChanged.connect(self.lcdNumber_minutes.display)
 
         QMetaObject.connectSlotsByName(Form)
     # setupUi
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"\u0423\u0441\u0442\u0430\u043d\u043e\u0432\u043a\u0430 \u0442\u0430\u0439\u043c\u0435\u0440\u0430 \u043f\u043e \u0432\u0440\u0435\u043c\u0435\u043d\u0438:", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"\u0412\u0440\u0435\u043c\u044f \u0442\u0430\u0439\u043c\u0435\u0440\u0430 \u0432 \u043c\u0438\u043d\u0443\u0442\u0430\u0445: ", None))
         self.label.setText(QCoreApplication.translate("Form", u"\u0413\u0440\u043e\u043c\u043a\u043e\u0441\u0442\u044c:", None))
-        self.label_4.setText(QCoreApplication.translate("Form", u"\u0438\u043b\u0438 \u0432\u0440\u0435\u043c\u044f \u043e\u0442\u043a\u043b\u044e\u0447\u0435\u043d\u0438\u044f:", None))
-        self.label_3.setText(QCoreApplication.translate("Form", u"TextLabel", None))
+        self.label_status.setText(QCoreApplication.translate("Form", u"TextLabel", None))
         self.pushButton_OK.setText(QCoreApplication.translate("Form", u"\u0421\u0442\u0430\u0440\u0442", None))
         self.pushButton_Cancel.setText(QCoreApplication.translate("Form", u"\u041e\u0442\u043c\u0435\u043d\u0430", None))
     # retranslateUi
